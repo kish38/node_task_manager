f32c654f94605f32deb1b7f83d5ae741a9066588
kish38 <kishorekumarnetala@gmail.com>
2019-04-23 10:48:45 +0530

    patch and delete tasks, users

--- src/index.js
+++ src/index.js
@@ -76,6 +76,18 @@ app.patch('/users/:id', async (req, res)=>{
     }
 })
 
+app.delete('/users/:id', async (req, res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
 app.get('/tasks', (req, res)=>{
     Task.find({}).then((tasks)=>{
         res.send(tasks)
@@ -94,7 +106,37 @@ app.get('/tasks/:id', (req, res)=>{
     }).catch((e)=>{
         res.status(500).send(e)
     })
-}) 
+})
+
+app.patch('/tasks/:id', async (req, res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdatesArray = ["description", "completed"]
+    const isValid = updates.every( (key) => allowedUpdatesArray.includes(key) )
+    if(!isValid){
+        return res.status(400).send({"error": "Invalid updates"})
+    }
+    try{
+        const user = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
 
 app.listen(port, ()=>{
     console.log("Server is on port", port)
