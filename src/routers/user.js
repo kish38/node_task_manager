67033791c30d9d4c6797a1b58358642ceb67d82c
kish38 <kishorekumarnetala@gmail.com>
2019-05-02 15:39:00 +0530

    middleware and user logout

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res)=>{
@@ -26,12 +27,31 @@ router.post('/users/login', async (req, res)=>{
     }
 })
 
-router.get('/users', (req, res)=>{
-    User.find({}).then((users)=>{
-        res.send(users)
-    }).catch((e)=>{
+router.post('/users/logout', auth, async (req, res)=>{
+    try{
+        req.user.tokens = req.user.tokens.filter((token)=>{
+            return token.token != req.token
+        })
+        await req.user.save()
+        res.send()
+    }catch(e){
         res.status(500).send()
-    })
+    }
+})
+
+router.post('/users/logoutAll', auth, async(req, res)=>{
+    try{
+        req.user.tokens = []
+        req.user.save()
+        res.send()
+    }
+    catch(e){
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res)=>{
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res)=>{
